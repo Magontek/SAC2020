088aef0eb9a2d01e6cb17907653d6e6d35615229
squash <squash@waldo>
2020-09-30 20:52:05 -0300

    cambiando el directorio de las imagenes a assets/

--- game.js
+++ game.js
@@ -18,11 +18,9 @@ var config = {
 
     function preload ()
     {
-        this.load.setBaseURL('http://labs.phaser.io');
-
-        this.load.image('sky', 'assets/skies/space3.png');
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-        this.load.image('red', 'assets/particles/red.png');
+        this.load.image('fondo', 'assets/space3.png');
+        this.load.image('logo', 'assets/logo.png');
+        this.load.image('red', 'assets/red.png');
     }
 
     function create ()
