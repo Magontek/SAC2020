8e4c4b09439801f9c5eed316a7d810c203571e06
Timmy <timmy.burton@hotmail.com>
2020-10-02 16:25:02 -0300

    agregado menu1

--- scenes/menu1.js
+++ scenes/menu1.js
@@ -2,6 +2,7 @@ class title extends Phaser.Scene {
 
         constructor () {
                 super({key: 'title'});
+        }
 
         preload () {
         this.load.image('fondo', 'assets/fondo_menu.jpg');
@@ -10,5 +11,7 @@ class title extends Phaser.Scene {
         create () {
                 let background = this.add.sprite(0, 0, 'fondo')
                 background.setOrigin(0, 0);
+        }
 }
+
 export default title;
