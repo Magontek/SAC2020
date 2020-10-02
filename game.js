1b41e0ccc2f93e23710ca4fa3c4fde6581322fbb
squash <squash@waldo>
2020-10-02 00:06:15 -0300

    Rama de testeo para verificar portabilidad desde sandbox https://phaser.io/sandbox/edit/1 a este sitio

--- game.js
+++ game.js
@@ -1,46 +1,68 @@
 var config = {
-        type: Phaser.AUTO,
-        width: 940,
-        height: 528,
-        physics: {
-            default: 'arcade',
-            arcade: {
-                gravity: { y: 200 }
-            }
-        },
-        scene: {
-            preload: preload,
-            create: create
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#000000',
+    parent: 'phaser-example',
+    physics: {
+        default: 'matter',
+        matter: {
+            gravity: {
+                scale: 1
+            },
+            plugins: {
+                attractors: true
+            }          
         }
-    };
-
-    var game = new Phaser.Game(config);
-
-    function preload ()
-    {
-        this.load.image('fondo', 'assets/fondo_menu.jpg');
-        this.load.image('logo', 'assets/logo.png');
-        this.load.image('red', 'assets/red.png');
+        
+    },
+    scene: {
+        preload: preload,
+        create: create
     }
+};
+var game = new Phaser.Game(config);
 
-    function create ()
-    {
-        this.add.image(470, 264, 'fondo');
+function preload ()
+{
+    this.load.image('sun', 'assets/sun.png');
+    this.load.image('alien', 'assets/space-baddie.png');
+    Text = this.add.text(16, 16, 'distancia', { fontSize: '32px', fill: '#fff' });
+    
+}
 
-        var particles = this.add.particles('red');
+function create ()
+{
+    //  You can enable the Attractors plugin either via the game config (see above), or explicitly in code:
+    // this.matter.enableAttractorPlugin();
+   
 
-        var emitter = particles.createEmitter({
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        });
+    this.matter.world.setBounds();
 
-        var logo = this.physics.add.image(400, 100, 'logo');
+    var sat = this.matter.add.sprite(10,10,'alien', null, {
+        mass: 1,
+        ignorePointer: true
+    });
+    
+    var sun = this.matter.add.sprite(400, 200, 'sun', null, {
+        shape: {
+            type: 'circle',
+            radius: 64
+        },
+        plugin: {
+            //attractors: [Phaser.Physics.Matter.Matter.Plugin.resolve(“matter-attractors”).Attractors.gravity]
+            attractors: [
+                function (bodyA, bodyB) {
+                    return {
+                        x: (bodyA.position.x - bodyB.position.x) * 0.000001,
+                        y: (bodyA.position.y - bodyB.position.y) * 0.000001
+                    };
+                }
+            ]
+        }
+    });
 
-        logo.setVelocity(100, 200);
-        logo.setBounce(1, 1);
-        logo.setCollideWorldBounds(true);
-
-        emitter.startFollow(logo);
-    }
-
+    this.matter.add.mouseSpring();
+    
+    Text.setText('Distancia: ' + sun.x);
+}
