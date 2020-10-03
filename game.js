fe370e262a3e6fca9b0bb43d939ebd22379377a1
squash <squash@waldo>
2020-10-03 13:22:58 -0300

    Saque el cogigo del juego que va en el cs_plat.js

--- game.js
+++ game.js
@@ -1,3 +1,16 @@
+// configuracion del juego
+var config = {
+        type: Phaser.AUTO,
+        width: 940,
+        height: 528,
+        parent: 'SAC2020',
+        scene: [sc_menu,sc_play], //ESTO ES NECESARIO, SE AGREGAN TODAS LAS ESCENAS ACÁ
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 0 }
+            }
+        },
+};
 
-
-var game = new Phaser.Game(config);
+    var game = new Phaser.Game(config);
