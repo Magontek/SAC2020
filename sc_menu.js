6803c4f9e421b704aedf2e70524cfd3932ad2f5a
Timmy <timmy.burton@hotmail.com>
2020-10-04 17:44:39 -0300

    Botones + fondo menu, v3

--- sc_menu.js
+++ sc_menu.js
@@ -5,7 +5,7 @@ var sc_menu = new Phaser.Class({
             Phaser.Scene.call(this, { key: 'sc_menu'});
         },
     preload (){
-        this.load.image('boton', 'assets/button.png')
+        this.load.image('menufondo', 'assets/menu_background.jpg')
         this.load.image('play', 'assets/boton_play.png')
         this.load.image('sound', 'assets/boton_sonido.png')
         this.load.image('soundbars', 'assets/barras_sonido.png')
@@ -13,6 +13,12 @@ var sc_menu = new Phaser.Class({
     },
     create(){
         this.input.mouse.disableContextMenu();
+
+        //BACKGROUND
+        var background = this.add.sprite(0, 0, 'menufondo');
+        background.setOrigin(0, 0);
+        background.setScale(0.979166667,0.843450479);
+
         texto = this.add.text(game.config.width / 2, 150, 'Scrap Pusher RL', {
             fontSize: '60px',
             fill: '#ffffff'
@@ -49,7 +55,7 @@ var sc_menu = new Phaser.Class({
         });
 
         //OPCION EXPLICACION Y CREDITOS
-        var opcion3 = this.add.sprite(300, 600, 'information').setInteractive();
+        var opcion3 = this.add.sprite(0, 500, 'information').setOrigin(0,0).setInteractive();
         opcion3.on('pointerover', function() {
             opcion3.setTint(0x828282);
         });
