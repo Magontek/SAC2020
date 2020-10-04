795e209f0c65eefdc57da7689a0b2f049df3a9a0
Timmy <timmy.burton@hotmail.com>
2020-10-04 19:53:13 -0300

    End screen work v2

--- sc_game_over.js
+++ sc_game_over.js
@@ -10,6 +10,32 @@ var sc_game_over = new Phaser.Class({
     },
 
     create (){
+        texto1 = this.add.text(800, 150, '   GAME OVER\n Final Score: ' + score, {
+            fontSize: '40px',
+            fill: '#c96520'
+        });
+
+        //TEXTO CREDITOS
+        texto2 = this.add.text(30,920, 'Game ended beacuse...', {
+            fontSize: '30px',
+            fill: '#fdb083'
+        });
+
+        //BOTON RETRODECER
+        back = this.add.text(game.config.width -300, game.config.height - 50, 'GO TO MENU', {
+            fontSize: '40px',
+            fill: '#ffffff'
+        }).setOrigin(0.0).setInteractive();
+
+        back.on('pointerover', function() {
+            back.setTint(0x554bbb);
+        });
+        back.on('pointerout', function() {
+            back.clearTint();
+        });
+        back.on('pointerdown', ()=>{
+            this.scene.start('sc_menu')
+        });
 
     },
 
